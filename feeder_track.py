cffbda17ea1bb39f493b7aaedf5596aaeefa8fb5
Lorentz-Andrei Ciubotica <lorentz-andrei.ciubotica@eon-romania.ro>
2022-08-30 15:35:19 +0200

    config ext

--- feeder_track.py
+++ feeder_track.py
@@ -2,9 +2,27 @@
 # coding: utf-8
 
 #from config import *
-
 import paho.mqtt.client as mqtt
 import ssl,time, csv, os
+import configparser
+
+config = configparser.ConfigParser()
+config.read("feeder.conf")
+
+# parsed demo data
+
+# server_host = mqtt.infinimesh.app
+# server_port = 8883
+# use_tls = yes
+# CA = pki/tls-ca-bundle.pem
+# device_key = pki/truck04.key
+# device_crt = pki/truck04.crt
+# topic = devices/b0ababba-cd99-404d-b5a0-62036d8407d5/state/reported/delta
+# csv_feeder_file = track04_anver_leipzig_modif.csv
+
+hostserver=config.get("server1","server_host")
+hostport=config.get("server1","server_port")
+
 def on_connect(client, userdata, flags, rc):
     print("Result from connect: {}".format(mqtt.connack_string(rc)))
     # Check whether the result form connect is the CONNACK_ACCEPTED connack code
@@ -17,15 +35,12 @@ def publish_value(client, topic, value):
 
 #payload contruct
 
-
-
-
 if __name__ == "__main__":
     client = mqtt.Client(protocol=mqtt.MQTTv311)
     client.tls_set("pki/tls-ca-bundle.pem", "pki/truck04.crt", "pki/truck04.key", tls_version=ssl.PROTOCOL_TLSv1_2)
     client.tls_insecure_set(False)
     client.on_connect = on_connect
-    client.connect(host="mqtt.infinimesh.app", port=8883)
+    client.connect(host=hostserver, port=hostport)
     client.loop_start()
     topic="devices/b0ababba-cd99-404d-b5a0-62036d8407d5/state/reported/delta"
     print_message = "{}:# {}"
