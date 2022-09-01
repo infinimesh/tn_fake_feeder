46f6305a5f568405bfd13a7beefd1b9fca74d72b
Mik <info@slnt-opp.xyz>
2022-09-01 12:19:16 +0200

    Add Statuses:  - online 80%  - inactive 10%  - offline 7%  - dead_offline 3%

--- go.mod
+++ go.mod
@@ -4,14 +4,15 @@ go 1.19
 
 require (
 	github.com/infinimesh/proto v0.0.0-20220714085139-ef57e42cbc97
+	github.com/mroth/weightedrand v0.4.2-0.20220430023629-058185e9b5eb
 	google.golang.org/grpc v1.49.0
+	google.golang.org/protobuf v1.28.0
 	gopkg.in/yaml.v2 v2.4.0
 	gorm.io/driver/sqlite v1.3.6
 	gorm.io/gorm v1.23.8
 )
 
 require (
-	github.com/fatih/structs v1.1.0 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/grpc-ecosystem/grpc-gateway/v2 v2.10.3 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
@@ -21,5 +22,4 @@ require (
 	golang.org/x/sys v0.0.0-20220627191245-f75cf1eec38b // indirect
 	golang.org/x/text v0.3.7 // indirect
 	google.golang.org/genproto v0.0.0-20220627200112-0a929928cb33 // indirect
-	google.golang.org/protobuf v1.28.0 // indirect
 )
--- pkg/common/trucks.go
+++ pkg/common/trucks.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	"github.com/infinimesh/tn_fake_feeder/pkg/db"
+	wr "github.com/mroth/weightedrand"
 )
 
 type Truck struct {
@@ -23,8 +24,9 @@ type Truck struct {
 var TN_TIME_FORMAT = "2006-02-01T15:04:05.999Z"
 
 type TruckReport struct {
-	Gps  []float64 `json:"gps"`
-	Sent string    `json:"sent"`
+	Gps    []float64 `json:"gps"`
+	Sent   string    `json:"sent"`
+	Status string    `json:"status"`
 }
 
 func (t *Truck) Stop() {
@@ -35,18 +37,59 @@ func (t *Truck) Start(wg *sync.WaitGroup) {
 	t.stop = false
 	t.wg = wg
 
+	status := t.Status()
+	hold := 0
 	for {
+
+		if status.SkipMove {
+			t.Point -= 1
+		}
+
 		np, n := t.Move(t.Point)
 		fmt.Printf("Moving Truck %s to Point %d(%.4f, %.4f)\n", t.Uuid, t.Point, np.Lat, np.Lng)
-		t.Report(t.Uuid, TruckReport{
-				Gps:    []float64{np.Lat, np.Lng},
-
 		t.Point = n
+
+		if status.Report {
+			t.Report(t.Uuid, TruckReport{
+				Gps:    []float64{np.Lat, np.Lng},
+				Sent:   time.Now().Format(TN_TIME_FORMAT),
+				Status: status.Key,
+			})
+		} else {
+			fmt.Printf("Truck(%s) is %s, won't report\n", t.Uuid, status.Key)
+		}
+
 		time.Sleep(t.Speed)
 		if t.stop {
 			break
 		}
+		if status.HoldStatus > hold {
+			fmt.Printf("Truck(%s) is in status %s, holding %d(%d left)\n", t.Uuid, status.Key, status.HoldStatus, status.HoldStatus-hold)
+			hold++
+		} else {
+			status = t.Status()
+			hold = 0
+		}
 	}
 
 	wg.Done()
+}
+
+type StatusWithProb struct {
+	Key        string
+	SkipMove   bool
+	Report     bool
+	HoldStatus int
+}
+
+var status_chooser, _ = wr.NewChooser(
+	wr.Choice[StatusWithProb]{Item: StatusWithProb{"online", false, true, 0}, Weight: 80},
+	wr.Choice[StatusWithProb]{Item: StatusWithProb{"inactive", true, true, 20}, Weight: 10},
+	wr.Choice[StatusWithProb]{Item: StatusWithProb{"offline", false, true, 10}, Weight: 7},
+	wr.Choice[StatusWithProb]{Item: StatusWithProb{"dead_offline", true, false, 10}, Weight: 3},
+)
+
+func (t *Truck) Status() StatusWithProb {
+	s := status_chooser.Pick()
+	return s
 }
